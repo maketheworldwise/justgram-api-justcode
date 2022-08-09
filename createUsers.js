9d8288f3431f9dd1e4d552adb455c15546f30c1b
Kevin Ahn <maketheworldwise@gmail.com>
2022-08-09 13:35:41 +0900

    :sparkles: 회원가입 API 구현

---
+++ createUsers.js
@@ -0,0 +1,38 @@
+const users = [
+  {
+    id: 1,
+    name: "Rebekah Johnson",
+    email: "Glover12345@gmail.com",
+    password: "123qwe",
+  },
+  {
+    id: 2,
+    name: "Fabian Predovic",
+    email: "Connell29@gmail.com",
+    password: "password",
+  },
+];
+
+const createUsers = (req, res) => {
+  const user = req.body.user;
+  users.push(user);
+
+  res.json({
+    message: "USER_CREATED",
+  });
+};
+
+module.exports = { createUsers };
+
+/*
+POST http://localhost:8000/signup
+
+{
+    "user" :   {
+        "id" : 3,
+        "name" : "Kevin Ahn",
+        "email" : "maketheworldwise@gmail.com",
+        "password" : "password"
+  }
+}
+*/
